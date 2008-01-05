90f57f6fa29f5195bbb2021eb300447e65112fad
hackovic <hackovic@c923623f-9e1f-0410-ae7c-bda1c0925892>
2008-01-05 17:10:13 +0000

    Langage files - use constants with sime name for labals in generated forms.

---
+++ trunk/lang/Bosanski.php
@@ -0,0 +1,19 @@
+<?php
+ 
+
+ ///labels for rezultat_select_edit.php
+ 
+ $LB_VIEWCHANGE_BTN = 'Vidi / Izmijeni';
+ 
+ $LB_DELETE_BTN			= 'Briši';
+ $LB_DELETE_BTN_CONFIRM = '\"Ovim potezom cete izbrisati izabranu opciju!\\n\\n Zelite li to da uradite?\"';
+ 
+ $LB_ADDTOLIST_BTN			= 'Prazan formular';
+ $LB_ADDTOLIST_BTN_TITLE= 'Dodaj novo na listu';
+ 
+ $LB_ADD_BTN			= 'Pošalji';
+ $LB_RESET_BTN		= 'Vrati na početak';
+ 
+ $LB_SOURCE_CODE  = '<b>PHP kod</b>: (kopiraj ovaj kod u tvoj .php fil)';
+ 
+ ?>
---
+++ trunk/lang/English.php
@@ -0,0 +1,19 @@
+ <?php
+ 
+
+ ///labels for rezultat_select_edit.php
+ 
+ $LB_VIEWCHANGE_BTN = 'View/Change';
+ 
+ $LB_DELETE_BTN			= 'Delete';
+ $LB_DELETE_BTN_CONFIRM = '\"Ovim potezom cete izbrisati izabranu opciju!\\n\\n Zelite li to da uradite?\"';
+ 
+ $LB_ADDTOLIST_BTN			= 'Empty form';
+ $LB_ADDTOLIST_BTN_TITLE= 'Add new item to list';
+ 
+ $LB_ADD_BTN			= 'Save';
+ $LB_RESET_BTN		= 'Reset';
+ 
+ $LB_SOURCE_CODE  = '<b>Source code</b>: (copy it into your .php file)';
+ 
+ ?>
