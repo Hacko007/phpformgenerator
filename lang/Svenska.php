912d6c728a5631b57df194bbb29b5831689d9c12
hackovic <hackovic@c923623f-9e1f-0410-ae7c-bda1c0925892>
2008-06-15 12:47:36 +0000

    add Swedish language support

---
+++ lang/Svenska.php
@@ -0,0 +1,19 @@
+﻿<?php
+ 
+
+ ///labels for rezultat_select_edit.php
+ 
+ $LB_VIEWCHANGE_BTN = 'Visa/Ändra';
+ 
+ $LB_DELETE_BTN			= 'Radera';
+ $LB_DELETE_BTN_CONFIRM = '\"Är du säker att du vill radera?\"';
+ 
+ $LB_ADDTOLIST_BTN			= 'Tom formulär';
+ $LB_ADDTOLIST_BTN_TITLE= 'Lägg till ny rad i listan';
+ 
+ $LB_ADD_BTN			= 'Spara';
+ $LB_RESET_BTN		= 'Ångra';
+ 
+ $LB_SOURCE_CODE  = '<b>Källkod</b>: (kopiera det här i din .php fil)';
+ 
+?>
